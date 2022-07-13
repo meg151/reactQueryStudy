a8f4c0078bb3790e607c069a044d3be953e515f6
meg151 <meg101810@gmail.com>
2022-07-13 13:01:30 +0900

    react query keepPreviousData

--- src/components/Planets/Planets.js
+++ src/components/Planets/Planets.js
@@ -1,39 +1,54 @@
-import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 import Planet from 'components/Planet/Planet';
-const queryClient = new QueryClient();
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-const fetchPlanets = async (key, greeting) => {
-  console.log(greeting);
-  const res = await fetch('http://swapi.dev/api/planets/');
-  return res.json();
-};
+const fetchPlanets = (page = 0) =>
+  fetch(`http://swapi.dev/api/planets/?page=` + page).then((res) => res.json());
 
 const Planets = () => {
-  const { data, status } = useQuery(['planets', 'hello, world'], fetchPlanets);
-  console.log(data);
+  const [page, setPage] = useState(1);
+  const { isLoading, isError, error, data, isFetching, isPreviousData } =
+    useQuery(['planets', page], () => fetchPlanets(page), {
+      keepPreviousData: true,
+    });
 
   return (
     <div>
       <h2>Planets</h2>
-      {/* <p>{status}</p> */}
-
-      {status === 'error' && <div>Error fetching data</div>}
-      {status === 'loading' && <div>Loading data...</div>}
-      {status === 'success' && (
-        <div>
-          {data.results.map((planet) => (
-            <Planet key={planet.name} planet={planet} />
-          ))}
-        </div>
-      )}
+      <div>
+        {isLoading ? (
+          <div>Loading...</div>
+        ) : isError ? (
+          <div>Error: {error.message} </div>
+        ) : (
+          <div>
+            {data.results.map((planet) => (
+              <Planet key={planet.name} planet={planet} />
+            ))}
+          </div>
+        )}
+        <span>Current Page: {page}</span>
+        <button
+          onClick={() => setPage((old) => Math.max(old - 1, 0))}
+          disabled={page === 0}
+        >
+          Previous Page
+        </button>{' '}
+        <button
+          onClick={() => {
+            if (!isPreviousData && data.hasMore) {
+              setPage((old) => old + 1);
+            }
+          }}
+          // Disable the Next Page button until we know a next page is available
+          disabled={isPreviousData || !data?.hasMore}
+        >
+          Next Page
+        </button>
+        {isFetching ? <span> Loading...</span> : null}{' '}
+      </div>
     </div>
   );
 };
 
-export default function Wraped() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Planets />
-    </QueryClientProvider>
-  );
-}
+export default Planets;
